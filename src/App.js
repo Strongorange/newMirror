a554459278b3569915a67e9df5fe99c6925d61e8
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-04-11 18:56:14 +0900

    [MODIFY] 사진 4개 그리드

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const GridContainer = styled.div`
   width: 100%;
   height: 100%;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
   color: rgba(255, 255, 255, 0.8);
 
   .item {
@@ -44,31 +44,33 @@ const GridContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    width: 100%;
+    height: 100%;
 
     &:nth-child(1) {
-      grid-row: 1 / 3;
-      margin-top: 40px;
+      grid-row: 1 / 2;
     }
 
     &:nth-child(2) {
-      grid-row: 1 / 4;
-      /* background-color: teal; */
+      grid-row: 1 / 2;
     }
     &:nth-child(3) {
-      /* background-color: green; */
-      grid-row: 3 / -1;
-      margin-top: 40px;
-    }
-    &:nth-child(4) {
-      /* background-color: purple; */
-      grid-row: 4 / -1;
-      margin-top: -30px;
+      grid-row: 2 / 3;
+      grid-column: 1 / 3;
     }
   }
 `;
 
 const GridItem = styled.div`
   position: relative;
+
+  &.photo {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+
+    gap: 5px;
+  }
 
   .clock-date {
     font-size: 30px;
@@ -93,30 +95,13 @@ const GridItem = styled.div`
   }
 `;
 
-const SubTitleContainer = styled.div`
+const PhotoWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 80%;
-  position: absolute;
-  top: 10%;
-`;
-
-const SubTitle = styled.span`
-  font-size: 20px;
-  color: rgba(255, 255, 255, 0.6);
-`;
-
-const Seperator = styled.div`
-  width: 100%;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.3);
 `;
 
 const WeatherContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 50px;
   width: 80%;
 `;
 
@@ -159,60 +144,14 @@ const WeatherIcon = styled.img`
   height: 100px;
 `;
 
-const ScheduleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100%;
-  height: 80%;
-  margin-top: 70px;
-  /* background-color: teal; */
-`;
-
-const ScheduleRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 80%;
-  color: rgba(255, 255, 255, 0.8);
-  justify-content: space-between;
-`;
-
-const ScheduleItem = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 10px;
-  gap: 10px;
-  span {
-    font-size: 24px;
-  }
-`;
-
-const PhotoContainer = styled.div`
-  display: flex;
-  width: 100%;
-  margin-top: -10px;
-`;
-
-const PhotoRow = styled.div`
-  display: flex;
-  width: 100%;
-  height: 200px;
-  justify-content: space-evenly;
-  /* background-color: teal; */
-`;
-
 const PhotoBox = styled.div`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 100%;
 `;
 
 const PhotoItem = styled.img`
-  width: 160px;
-  height: 160px;
+  width: 100%;
+  height: 100%;
 `;
 
 function App() {
@@ -297,10 +236,6 @@ function App() {
                   />
                 </GridItem>
                 <GridItem className="item">
-                  <SubTitleContainer className="weather">
-                    <SubTitle>군산</SubTitle>
-                    <Seperator />
-                  </SubTitleContainer>
                   <WeatherContainer>
                     <WeatherRow>
                       <WeatherItem>
@@ -337,53 +272,19 @@ function App() {
                     </WeatherRow>
                   </WeatherContainer>
                 </GridItem>
-                <GridItem className="item">
-                  <SubTitleContainer className="schedule">
-                    <SubTitle>우리 일정!</SubTitle>
-                    <Seperator />
-                  </SubTitleContainer>
-                  <ScheduleContainer>
-                    {schedules !== null ? (
-                      schedules.map((c, i) => (
-                        <ScheduleRow key={i}>
-                          <ScheduleItem>
-                            <Text>{`${new Date(
-                              c.date.seconds * 1000
-                            ).getFullYear()}-${
-                              new Date(c.date.seconds * 1000).getMonth() + 1
-                            }-${new Date(
-                              c.date.seconds * 1000
-                            ).getDate()}`}</Text>
-                          </ScheduleItem>
-                          <ScheduleItem>
-                            <Text>{c.name}</Text>
-                          </ScheduleItem>
-                        </ScheduleRow>
-                      ))
-                    ) : (
-                      <ScheduleRow>
-                        <ScheduleItem>
-                          <Text>일정 없음!</Text>
-                        </ScheduleItem>
-                      </ScheduleRow>
-                    )}
-                  </ScheduleContainer>
-                </GridItem>
-                <GridItem className="item">
-                  <SubTitleContainer className="weekly">
-                    <SubTitle>RUZA's PICK</SubTitle>
-                    <Seperator />
-                    <PhotoContainer>
-                      <PhotoRow>
-                        <PhotoBox>
-                          <PhotoItem src={gallery[0]} />
-                        </PhotoBox>
-                        <PhotoBox>
-                          <PhotoItem src={gallery[1]} />
-                        </PhotoBox>
-                      </PhotoRow>
-                    </PhotoContainer>
-                  </SubTitleContainer>
+                <GridItem className="item photo">
+                  <PhotoBox>
+                    <PhotoItem src={gallery[0]} />
+                  </PhotoBox>
+                  <PhotoBox>
+                    <PhotoItem src={gallery[1]} />
+                  </PhotoBox>
+                  <PhotoBox>
+                    <PhotoItem src={gallery[0]} />
+                  </PhotoBox>
+                  <PhotoBox>
+                    <PhotoItem src={gallery[1]} />
+                  </PhotoBox>
                 </GridItem>
               </GridContainer>
             </>
