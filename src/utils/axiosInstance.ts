ca80bbb09a85ae1ff1574dafe194d95a1c6a312b
Chanhwi Lee <74127841+Strongorange@users.noreply.github.com>
2025-06-18 15:09:56 +0900

    [fix] OpenWeather OneCall API 2.5 -> 3.0 마이그레이션

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const weatherInstance = axios.create({
-  baseURL: "https://api.openweathermap.org/data/2.5/onecall",
+  baseURL: "https://api.openweathermap.org/data/3.0/onecall",
   timeout: 4000,
 });
 
